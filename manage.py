c470bf25b3d0a363f3f7f2f0383d39faf6ee64c3
Lorenzo <lorenzo.lancia@eni.com>
2020-12-12 23:57:15 +0100

    changed dockerfile to use integrade flask dev server

--- manage.py
+++ manage.py
@@ -15,6 +15,9 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+@manager.command
+def runserver():
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
