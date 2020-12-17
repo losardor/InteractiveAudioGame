773bdb03bc2ed1a9a45d504d1717d73bd5241637
Lorenzo <lorenzo.lancia@eni.com>
2020-12-17 23:24:46 +0100

    work in progress

--- app/books/view.py
+++ app/books/view.py
@@ -21,14 +21,24 @@ def index():
     return render_template('books/index.html', books=thebooks)
 
 
-@books.route('/info/<int:book_id>')
+@books.route('/book/<int:book_id>')
 @login_required
 def book_info(book_id):
     """view a book page"""
     book = Book.query.filter_by(id=book_id).first()
     if book is None:
         abort(404)
-    return render_template('books/book_info.html', book=book)
+    return render_template('books/waypoint.html', book=book)
+
+@books.route('/book/<int:book_id>/waypoint/<int:waypoint>')
+@login_required
+def waypoint(book_id, waypoint):
+    """view a book page"""
+    book = Book.query.filter_by(id=book_id).first()
+    if book is None:
+        abort(404)
+    return render_template('books/waypoint.html', book=book, waypoint=waypoint)
+
 
 
 
